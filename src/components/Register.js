12cf9d912d5a6dcf373bf204a9db311cb0cbe91e
Shanzie <bladeshill@gmail.com>
2023-12-24 18:00:39 +1000

    heroku deployment

--- src/components/Register.js
+++ src/components/Register.js
@@ -14,7 +14,7 @@ function Register() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:3000/register', {
+      const response = await fetch('https://workplacerostering.herokuapp.com/api/endpoint', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
