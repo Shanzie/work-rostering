ea260a48ccfbca4c6d42ff88f12ea157efd733d2
Shanzie <bladeshill@gmail.com>
2023-12-24 17:26:47 +1000

    password encryption

--- routes/register.js
+++ routes/register.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User'); 
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
@@ -11,7 +12,14 @@ router.post('/register', async (req, res) => {
       return res.status(400).json({ msg: 'User already exists' });
     }
 
-    user = new User({ name, email, password });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    user = new User({
+      name,
+      email,
+      password: hashedPassword
+    });
     await user.save();
 
     res.status(201).json({ msg: 'User registered successfully' });
